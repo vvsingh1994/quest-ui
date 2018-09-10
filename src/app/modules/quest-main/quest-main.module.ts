78fbf96288739a1d0689de137d2f4a5f74f59c75
Amit Maurya <amitkumar.maurya1993@gmail.com>
2018-09-10 21:33:09 +0530

    Added profile details module basic structure

--- src/app/modules/quest-main/quest-main.module.ts
+++ src/app/modules/quest-main/quest-main.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { QuestToolbarModule } from '../quest-toolbar/quest-toolbar.module';
 import { QuestFeedModule } from '../quest-feed/quest-feed.module';
 import { QuestMainComponent } from './quest-main.component';
+import { QuestProfileModule } from '../quest-profile/quest-profile.module';
 @NgModule({
   imports: [
     CommonModule,
     QuestToolbarModule,
-    QuestFeedModule
+    QuestFeedModule,
+    QuestProfileModule
   ],
   declarations: [QuestMainComponent],
   exports: [QuestMainComponent]
--- src/app/modules/quest-profile/quest-profile.module.ts
+++ src/app/modules/quest-profile/quest-profile.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ProfileDetailComponent } from './components/profile-detail/profile-detail.component';
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ProfileDetailComponent],
+  exports: [ProfileDetailComponent]
 })
 export class QuestProfileModule { }
